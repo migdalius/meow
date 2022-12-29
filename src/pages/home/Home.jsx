70f4ca9e49bec0bb6648547681e0fce270859869
migdalius <30595371+migdalius@users.noreply.github.com>
2022-12-29 11:39:29 +0100

    footer component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import "../home/home.css";
 import Navigation from "../../components/nav/Navigation";
 import Hero from "../../components/hero/Hero";
+import Footer from "../../components/footer/Footer";
 const Home = () => {
   return (
     <div className="app">
       <Navigation />
       <Hero />
+      <Footer />
     </div>
   );
 };
